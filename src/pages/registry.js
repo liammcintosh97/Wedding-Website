e550f8aa9be0f1eda260f9a772826bc13c50b4e2
Liam McIntosh <liam.ed.mc97@gmail.com>
2022-01-21 10:16:32 +1100

    fixed Event Tool Tip Jittering bug

--- src/pages/registry.js
+++ src/pages/registry.js
@@ -3,6 +3,18 @@ import React from "react"
 import "./styles/registry.scss"
 
 class Registry extends React.Component{
+
+  componentDidMount () {
+    const script = document.createElement("script");
+    const container = document.getElementById("registry-container");
+
+    script.src = "//www.myregistry.com//Visitors/GiftList/iFrames/EmbedRegistry.ashx?r=ZDkYyG_QHLqfgGQe5NKUVQ2&v=2";
+    script.type= "text/javascript"
+    script.id="script_myregistry_giftlist_iframe"
+
+    container.appendChild(script);
+  }
+
   render(){
     return (
       <div className="registryPage-container">
@@ -10,8 +22,10 @@ class Registry extends React.Component{
           <h1>registry</h1>
         </header>
         <main>
-          <div>
-            <iframe title="Registry Frame" src="https://www.myregistry.com/wedding-registry/monique-borg-and-liam-mcintosh-bayswater-victoria/3070649"/>
+          <div class="registry-message">
+            <p>Our wedding registry is brought to you by MyRegistry.com you can add to it by following this <a href="https://www.myregistry.com/wedding-registry/monique-borg-and-liam-mcintosh-bayswater-victoria/3070649">link</a> or by interacting with this page</p>
+          </div>
+          <div id="registry-container">
           </div>
         </main>
         <footer>
